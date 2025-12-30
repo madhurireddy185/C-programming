76e031eda98f9a438ee1966f98a69e9043fa2093
Madhuri reddy <madhurireddy185@gmail.com>
2025-12-30 15:46:05 +0530

    Add files via upload
    print the numbers upto N

---
+++ print numbers from 1 to n.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int i,n;
+    printf("enter any number");
+    scanf("%d",&n);
+    printf("numbers from 1 to %d are");
+    for (i=1; i<=n; i++);
+    printf("%d \n",i);
+
+    
+    return 0;
+}
