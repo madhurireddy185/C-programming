6659be7cf8e4c21245e05c87b0244ff9fd68851e
Madhuri reddy <madhurireddy185@gmail.com>
2026-01-06 17:08:13 +0530

    Add files via upload
    to find average elements in array

---
+++ to find average of an array elements.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int sum = 0;
+    float average;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    average = (float)sum / n;
+    printf("Array elements: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\nSum of array elements: %d\n", sum);
+    printf("Average of array elements: %.2f\n", average);
+
+    return 0;
+}
