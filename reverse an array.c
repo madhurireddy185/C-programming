cf18899626248b46be7aab4417910575307f2efc
Madhuri reddy <madhurireddy185@gmail.com>
2026-01-06 17:03:32 +0530

    Add files via upload

---
+++ reverse an array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+    void reverseArray(int arr[],int start,int end){
+    int temp;
+    while (start<end){
+        temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+    }
+    }
+    void printArray(int arr[],int size){
+        for (int i=0;i<size;i++)
+        printf("%d",arr[i]);
+        printf("\\n");
+    }
+    int main(){
+        int arr[]={1,2,3,4};
+        int n=sizeof(arr)/sizeof(arr[0]);
+        printf("original array is:\\n");
+        printArray(arr,n);
+        reverseArray(arr,0,n-1);
+        printf("reversed array is:\\n");
+        printArray(arr,n);
+    
+    return 0;
+}
+}
