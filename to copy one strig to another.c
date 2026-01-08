84593205e48758fc30f052ecce2e418f00185e0c
Madhuri reddy <madhurireddy185@gmail.com>
2026-01-08 17:20:21 +0530

    Add files via upload
    to copy one string to another

---
+++ to copy one strig to another.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char source[] = "Hello";
+    char destination[20]; 
+    strcpy(destination, source);
+
+    printf("Source string: %s\n", source);
+    printf("Destination string: %s\n", destination);
+
+    return 0;
+}
