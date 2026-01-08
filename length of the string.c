17b72c11957f75b08e2c06de53403c87c3bdedba
Madhuri reddy <madhurireddy185@gmail.com>
2026-01-08 17:19:42 +0530

    Add files via upload
    to find the length of the string

---
+++ length of the string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[] = "Hello";
+    size_t length = strlen(str);
+
+    printf("The length of the string is: %zu\n", length);
+
+    return 0;
+}
