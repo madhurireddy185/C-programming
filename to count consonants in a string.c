475644ea8adfcb318ab92b024c4cdeb36d8ec7ba
Madhuri reddy <madhurireddy185@gmail.com>
2026-01-09 17:06:00 +0530

    Add files via upload
    to count consonents in string

---
+++ to count consonants in a string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+    int count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        char c = tolower(str[i]);
+        if (c >= 'a' && c <= 'z') {
+            if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') {
+                count++;
+            }
+        }
+    }
+
+    printf("Total consonants: %d\n", count);
+    return 0;
+}
