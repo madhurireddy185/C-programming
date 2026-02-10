76c7c4139df372aa7caff66b5e10367188bde6bb
Madhuri reddy <madhurireddy185@gmail.com>
2026-02-10 17:30:05 +0530

    Add files via upload
    switch cases

---
+++ employee salary calculation.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+ {
+    int code, experience;
+    int baseSalary = 0, bonus = 0, totalSalary = 0;
+    scanf("%d", &code);
+    scanf("%d", &experience);
+    switch(code)
+     {
+        case 1:
+            baseSalary = 50000;  
+            break;
+        case 2:
+            baseSalary = 35000;   
+            break;
+        default:
+            printf("Invalid designation code");
+            return 0;
+    }
+    if(experience >= 3) 
+    {
+        bonus = experience * 5000;
+    }
+    totalSalary = baseSalary + bonus;
+    printf("Salary ₹%d", totalSalary);
+    return 0;
+}
