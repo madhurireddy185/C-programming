3c524829eb705014187397f6d2a319fada2dedf9
Madhuri reddy <madhurireddy185@gmail.com>
2026-05-22 09:37:24 +0530

    Add files via upload
    looping problems

---
+++ sample problem 18.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	a =10;
+	
+	printf(" The value of a BEFORE increment is %d :\n",a);
+	
+	
+	a = a+1;
+	
+	printf(" The value of a AFTER increment is %d :\n",a);
+}
