e4584b8e5732ca188e589e06cca85a4b1999b82b
Madhuri reddy <madhurireddy185@gmail.com>
2026-05-22 09:35:54 +0530

    Add files via upload
    conditional statements

---
+++ sample problem 10.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	
+	int A =200000;
+	int B =100000;
+	
+	printf(" Result of addition %d :\n\n",A+B);
+	
+		printf(" Result of subtraction %d :\n\n",A-B);
+	
+		printf(" Result of multiplication %d :\n\n",A*B);
+	
+		printf(" Result of division %d :\n\n",A/B);
+	
+}
