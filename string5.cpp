399eea9ca142d289c97a6c601d8f6e3e5660675d
Madhuri reddy <madhurireddy185@gmail.com>
2026-05-25 14:51:07 +0530

    Add files via upload
    vowels and consonants

---
+++ string5.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char name[54]="sri shanmugha college of engineering and technology";
+  printf("%s\n\n",name);	
+	 printf("%s54\n\n",name);	
+	 printf("%s40\n\n",name);	
+	 printf("%s.40\n\n",name);	
+	 printf("%s10.5\n\n",name);	
+	 printf("%-10.5s\n\n",name);	
+	
+}
+	
+	
+
